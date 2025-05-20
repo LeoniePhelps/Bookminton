73d308f53216e0ee8c14b1a3b24503f13b441693
leoniephelps <leoniephelps96@gmail.com>
2025-05-20 20:50:25 +0100

    wip

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine($"Booking Badminton at {DateTime.Now}");
+
+using var client = new HttpClient();
+var authToken = Environment.GetEnvironmentVariable("AUTH_TOKEN");
+
+client.DefaultRequestHeaders.Add("Authorization", authToken);
+client.DefaultRequestHeaders.Add("Origin", "https://bookings.better.org.uk");
+
+var response = await client.GetAsync($"{Environment.GetEnvironmentVariable("SUGDEN_BADMINTON_URL")}2025-05-22");
+var content = await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(content);
+
+Console.WriteLine($"Badminton Booked at {DateTime.Now}");
